11fa7ebb49b55653f8c84912b56e3feaa871f620
Yash Lokwani <lokwaniyash2000@gmail.com>
2021-10-15 21:37:28 +0530

    solved puzzle 10 part 2

--- p10-2.js
+++ p10-2.js
@@ -6,8 +6,9 @@ let jolts = [
   130, 33, 20, 190, 35, 79, 162, 122, 98, 177, 179, 68, 48, 118, 125, 192, 174,
   99, 152, 3, 89, 105, 180, 191, 61, 13, 90, 129, 47, 138, 67, 115, 44, 59, 60,
   95, 93, 166, 154, 101, 34, 113, 139, 77, 94, 161, 187, 45, 22, 12, 163, 41,
-  27, 132, 30, 143, 168, 144, 83, 100, 102, 72,
+  27, 132, 30, 143, 168, 144, 83, 100, 102, 72, 0, 195,
 ];
+let diffStr = "";
 
 class Node {
   value = "";
@@ -53,12 +54,15 @@ for (let i = 1; i < jolts.length; i++) {
   if (jolts[i] - last == 1) {
     one++;
     last = jolts[i];
+    diffStr += "1";
   } else if (jolts[i] - last == 2) {
     two++;
     last = jolts[i];
+    diffStr += "2";
   } else if (jolts[i] - last == 3) {
     three++;
     last = jolts[i];
+    diffStr += "3";
   }
 }
 
@@ -71,9 +75,9 @@ for (let i = 1; i < jolts.length; i++) {
 //   }
 // }
 
-let allPaths = 0;
-let allFinalPaths = 0;
-joltTree(0);
+// let allPaths = 0;
+// let allFinalPaths = 0;
+// // joltTree(0);
 // let node = joltTree(new Node(jolts[0]));
 // printTree(
 //   node,
@@ -81,32 +85,84 @@ joltTree(0);
 //   (node) => node.children
 // );
 
-console.log(allPaths);
+// console.log(allPaths);
 console.log(last);
-// console.log(allFinalPaths);
+// // console.log(allFinalPaths);
 
-function joltTree(value) {
-  // console.log(node.value);
-  // if (node.value == last) {
-  //   // console.log("found it");
-  //   allPaths++;
-  // }
-  for (let i = 0; i < jolts.length; i++) {
-    if (jolts[i] - value >= 1 && jolts[i] - value <= 3) {
-      // let child = new Node(jolts[i]);
-      // node.addVal(joltTree(child));
-      // joltTree(child);
-      // console.log(jolts[i]);
-      if (jolts[i] == last) allPaths++;
-      joltTree(jolts[i]);
+// function joltTree(node) {
+//   // console.log(node.value);
+//   if (node.value == last) {
+//     // console.log("found it");
+//     allPaths++;
+//   }
+//   for (let i = 0; i < jolts.length; i++) {
+//     if (jolts[i] - node.value >= 1 && jolts[i] - node.value <= 3) {
+//       let child = new Node(jolts[i]);
+//       node.addVal(joltTree(child));
+//       // joltTree(child);
+//       // console.log(jolts[i]);
+//       // if (jolts[i] == last) allPaths++;
+//       // joltTree(jolts[i]);
+//     }
+//   }
+//   // console.log(node.value == last, last);
+//   // allFinalPaths++;
+//   return node;
+// }
+
+console.log(one, two, three);
+console.log(diffStr);
+
+function findRepetitions(str, n, char, surr = false) {
+  let total = 0;
+  let found = 0;
+  let s = 0,
+    e = 0,
+    started = false;
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] == str[i + 1] && str[i] == char) {
+      found++;
+      // console.log(i, str[i], "match");
+      if (!started) {
+        started = true;
+        s = i;
+      }
+    } else {
+      (s = 0), (e = 0), (started = false);
+      found = 0;
+    }
+
+    if (found + 1 == n) {
+      found = 0;
+      started = false;
+      e = i + 1;
+      // console.log(i, str[i], "total", str[s - 1], str[e + 1], s, e);
+
+      if (surr) {
+        if (
+          s != 0 &&
+          str[s - 1] == "3" &&
+          e != str.length - 1 &&
+          str[e + 1] == 3
+        ) {
+          total++;
+        }
+      } else {
+        total++;
+      }
+      i++;
     }
   }
-  // console.log(node.value == last, last);
-  // allFinalPaths++;
-  return;
+  return total;
 }
 
-// console.log(one, two, three);
+// console.log(findRepetitions(diffStr, 2, "1", true));
+
+let a = findRepetitions(diffStr, 4, "1"),
+  b = findRepetitions(diffStr, 3, "1", true),
+  c = findRepetitions(diffStr, 2, "1", true);
+
+console.log(7 ** a, 4 ** b, 2 ** c, 7 ** a * 4 ** b * 2 ** c, a, b, c);
 
 // for (let i = 0; i < node.children.length; i++) {
 //   console.log(i, node.children[i]);
